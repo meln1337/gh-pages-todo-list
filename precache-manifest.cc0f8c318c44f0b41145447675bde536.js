28cb15344cb572c6674ff06da72bed0938027455
meln1337 <53012422+meln1337@users.noreply.github.com>
2020-03-24 19:29:57 +0200

    Updates

---
+++ precache-manifest.cc0f8c318c44f0b41145447675bde536.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdd80521a2ce21fc3a247c659c03437",
+    "url": "/gh-pages-todo-list/index.html"
+  },
+  {
+    "revision": "9a0f92632581b2485945",
+    "url": "/gh-pages-todo-list/static/css/2.0e8603eb.chunk.css"
+  },
+  {
+    "revision": "ef928061de4ef22e6946",
+    "url": "/gh-pages-todo-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9a0f92632581b2485945",
+    "url": "/gh-pages-todo-list/static/js/2.77ee2b0d.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/gh-pages-todo-list/static/js/2.77ee2b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef928061de4ef22e6946",
+    "url": "/gh-pages-todo-list/static/js/main.46a30815.chunk.js"
+  },
+  {
+    "revision": "e417f6134c23e7741a6f",
+    "url": "/gh-pages-todo-list/static/js/runtime-main.b1732d94.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/gh-pages-todo-list/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/gh-pages-todo-list/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/gh-pages-todo-list/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/gh-pages-todo-list/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/gh-pages-todo-list/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/gh-pages-todo-list/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/gh-pages-todo-list/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/gh-pages-todo-list/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/gh-pages-todo-list/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/gh-pages-todo-list/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/gh-pages-todo-list/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/gh-pages-todo-list/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/gh-pages-todo-list/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/gh-pages-todo-list/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/gh-pages-todo-list/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/gh-pages-todo-list/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
